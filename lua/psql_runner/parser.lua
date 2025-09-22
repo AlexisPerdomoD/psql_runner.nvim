c82c2ff5b4d4f5097d385037b0ba8422a2d11a98
cuji <alexmetal66@gmail.com>
2025-09-22 09:09:47 -0500

    fix: correct type for parser

--- lua/psql_runner/parser.lua
+++ lua/psql_runner/parser.lua
@@ -1,23 +1,30 @@
 ---@class Parser
----@field parse_query_from_selection function -> string|nil
 local P = {}
 
 P.parse_query_from_selection = function()
     local start_pos = vim.fn.getpos "'<"
     local end_pos = vim.fn.getpos "'>"
 
-    if #start_pos < 2 or #end_pos < 2 then return nil end
+    if #start_pos < 2 or #end_pos < 2 then
+        return nil
+    end
 
     local lines = vim.fn.getline(start_pos[2], end_pos[2])
-    if type(lines) == 'string' then lines = table.insert({}, lines) end
+    if type(lines) == 'string' then
+        lines = table.insert({}, lines)
+    end
 
-    if #lines == 0 then return nil end
+    if #lines == 0 then
+        return nil
+    end
 
     lines[1] = string.sub(lines[1], start_pos[3], -1)
     lines[#lines] = string.sub(lines[#lines], 1, end_pos[3])
 
     local query = table.concat(lines, '\n')
-    if query == '' then return nil end
+    if query == '' then
+        return nil
+    end
     return query
 end
 
